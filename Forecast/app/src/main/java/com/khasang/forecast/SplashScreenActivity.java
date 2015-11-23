59071f3db6d0b2fefa5c34545fff0475a463e577
RomeoND <copypastestd@gmail.com>
2015-11-24 01:37:36 +0300

    add WeatherActivity

--- Forecast/app/src/main/java/com/khasang/forecast/SplashScreenActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.khasang.forecast;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        /** Заглушка для запуска WeatherActivity */
+        Intent intent = new Intent(this, WeatherActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Forecast/app/src/main/java/com/khasang/forecast/WeatherActivity.java
@@ -0,0 +1,15 @@
+package com.khasang.forecast;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class WeatherActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+
+    }
+}
