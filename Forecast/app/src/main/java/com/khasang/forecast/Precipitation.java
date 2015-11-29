ee7f53709d0bc64eed48f2c7f507eae61fc4f8e4
Veda <veda_ph@mail.ru>
2015-11-29 07:29:11 +0300

    add types of Precipitation (согласно http://openweathermap.org)

--- Forecast/app/src/main/java/com/khasang/forecast/Precipitation.java
+++ Forecast/app/src/main/java/com/khasang/forecast/Precipitation.java
@@ -1,5 +1,7 @@
 package com.khasang.forecast;
 
+import android.widget.Switch;
+
 /**
  * Created by Veda on 24.11.15.
  */
@@ -8,7 +10,26 @@ public class Precipitation {
     private Type type;
     private int probability;
 
-    public static enum Type {SUNNY, CLOUDS, RAIN, STORM}
+    public static enum Type {
+        CLEAR_SKY, FEW_CLOUDS, SCATTERED_CLOUDS, BROKEN_CLOUDS,
+        SHOWER_RAIN, RAIN, TRUNDERSTORM, SNOW, MIST;
+
+        @Override
+        public String toString() {
+            switch(this) {
+                case CLEAR_SKY: return "Ясно";
+                case FEW_CLOUDS: return "Небольшая облачность";
+                case SCATTERED_CLOUDS: return "Облачно";
+                case BROKEN_CLOUDS: return "Облачно с прояснениями";
+                case SHOWER_RAIN: return "Ливень";
+                case RAIN: return "Дождь";
+                case TRUNDERSTORM: return "Гроза";
+                case SNOW: return "Снег";
+                case MIST: return "Туман";
+                default: return "Type";
+            }
+        }
+    }
 
     public Precipitation(Type type, int probability) {
         this.type = type;
