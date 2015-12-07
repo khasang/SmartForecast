fffb901b3a598a0b5153e0c1872bb7605704e30d
DarkLab <allihov67@gmail.com>
2015-12-07 19:05:48 +0300

    небольшой откат

--- Forecast/app/src/main/java/com/khasang/forecast/activities/CityPickerActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/CityPickerActivity.java
@@ -162,7 +162,6 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
                 builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        PositionManager.getInstance().removePositions();
                         CityPickerActivity.this.clearList();
                         recyclerAdapter.notifyDataSetChanged();
                     }
