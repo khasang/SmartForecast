859c36534787668619f35fef734ae408cdf9b3f3
DarkLab <allihov67@gmail.com>
2015-12-16 22:36:52 +0300

    добавлен доступ к контексту приложения

--- Forecast/app/src/main/java/com/khasang/forecast/MyApplication.java
+++ Forecast/app/src/main/java/com/khasang/forecast/MyApplication.java
@@ -1,15 +1,22 @@
 package com.khasang.forecast;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by aleksandrlihovidov on 15.12.15.
  */
 public class MyApplication extends Application {
+    private static Context appContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        appContext = getApplicationContext();
         DrawUtils.getInstance().init(this);
+    }
+
+    public static Context getAppContext() {
+        return appContext;
     }
 }
