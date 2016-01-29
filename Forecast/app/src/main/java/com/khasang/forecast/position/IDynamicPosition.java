132a2451e0697e3c4645cd5b46f1478c43f05eba
RNOVOSELOV <novoselov_roman@mail.ru>
2016-01-29 16:34:12 +0300

    Удалил полуночные фантации

--- Forecast/app/src/main/java/com/khasang/forecast/position/IDynamicPosition.java
+++
@@ -1,8 +0,0 @@
-package com.khasang.forecast.position;
-
-/**
- * Created by roman on 29.01.16.
- */
-public interface IDynamicPosition {
-    void updateCoordinates ();
-}
--- Forecast/app/src/main/java/com/khasang/forecast/position/PositionManager.java
+++ Forecast/app/src/main/java/com/khasang/forecast/position/PositionManager.java
@@ -41,6 +41,10 @@ public class PositionManager {
     private WeatherActivity mActivity;
     private SQLiteProcessData dbManager;
     private boolean lastResponseIsFailure;
+    private LocationManager locationManager;
+    private CurrentLocationListener locationListener;
+    private boolean gps_enabled;
+    private boolean network_enabled;
 
     private static class ManagerHolder {
         private final static PositionManager instance = new PositionManager();
@@ -358,5 +362,70 @@ public class PositionManager {
             calendar.add(Calendar.DAY_OF_YEAR, DAY_PERIOD);
         }
         return forecast;
+    }
+
+    public void initLocationManager() {
+        locationManager = (LocationManager) MyApplication.getAppContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new CurrentLocationListener();
+    }
+
+    public boolean isLocationManagerWorks() {
+        gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        return (gps_enabled && network_enabled);
+    }
+
+    public void updateCurrentLocationCoordinates() {
+        if (ActivityCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (network_enabled) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+        if (gps_enabled) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    class CurrentLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location == null){
+                return;
+            }
+            if (ActivityCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MyApplication.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.removeUpdates(locationListener);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 }
