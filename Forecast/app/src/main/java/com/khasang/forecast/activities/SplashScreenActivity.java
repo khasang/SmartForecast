36121bee59ede9551786505493029e5aa2c9bfca
Роман Новоселов <novoselov_roman@mail.ru>
2016-03-12 02:23:07 +0300

    Доделал Сплэш как планировал

--- Forecast/app/src/main/java/com/khasang/forecast/activities/SplashScreenActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/SplashScreenActivity.java
@@ -7,13 +7,15 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -24,31 +26,49 @@ import com.khasang.forecast.interfaces.IPermissionCallback;
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerTextView;
 
-import static com.khasang.forecast.PermissionChecker.RuntimePermissions.PERMISSION_REQUEST_FINE_LOCATION;
+import net.frakbot.jumpingbeans.JumpingBeans;
 
 import java.io.IOException;
+import java.util.Calendar;
 
+import pl.droidsonroids.gif.AnimationListener;
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
 
-
 public class SplashScreenActivity
         extends AppCompatActivity
-        implements IPermissionCallback {
+        implements IPermissionCallback, Animation.AnimationListener, AnimationListener {
 
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private volatile boolean coordinatesServicesChecked = false;
     private volatile boolean runtimePermissionChecked = false;
     private volatile boolean gifIsFinishedPlaying = false;
 
-    GifDrawable gifDrawable = null;
-    Shimmer shimmer;
+    private GifDrawable gifDrawable = null;
+    private Shimmer shimmer;
+    private ShimmerTextView welcomeText;
+    private JumpingBeans jumpingBeans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        String text = getString(R.string.welcome_string_hello);
+        if (hour < 4) {
+        } else if (hour <= 10) {
+            text = getString(R.string.welcome_string_morning);
+        } else if (hour <= 17) {
+            text = getString(R.string.welcome_string_day);
+        } else if (hour <= 22) {
+            text = getString(R.string.welcome_string_evening);
+        }
+        welcomeText = ((ShimmerTextView) findViewById(R.id.welcomeText));
+        welcomeText.setText(text);
+        jumpingBeans = JumpingBeans
+                .with(welcomeText)
+                .appendJumpingDots()
+                .build();
         GifImageView gifImageView = ((GifImageView) findViewById(R.id.gifImageView));
         try {
             gifDrawable = new GifDrawable(getResources(), R.raw.splash_screen);
@@ -56,27 +76,24 @@ public class SplashScreenActivity
         } catch (IOException e) {
             e.printStackTrace();
         }
+        gifDrawable.addAnimationListener(this);
         gifImageView.setImageDrawable(gifDrawable);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        gifDrawable.start();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                gifIsFinishedPlaying = true;
-                startWeatherActivity();
-            }
-        }, gifDrawable.getDuration());
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.welcome_string_appear);
+        welcomeText.startAnimation(animation);
+        welcomeText.setVisibility(View.VISIBLE);
+        shimmer = new Shimmer();
+        shimmer.start(welcomeText);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        shimmer = new Shimmer();
-        shimmer.start(((ShimmerTextView) findViewById(R.id.welcomeText)));
+        gifDrawable.start();
         if (checkPlayServices()) {
             checkCoordinatesServices();
             checkPermissions();
@@ -86,13 +103,12 @@ public class SplashScreenActivity
     @Override
     protected void onPause() {
         super.onPause();
-        shimmer.cancel();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        gifDrawable.stop();
+        jumpingBeans.stopJumping();
     }
 
     private boolean checkProviders() {
@@ -132,7 +148,7 @@ public class SplashScreenActivity
     private void checkPermissions() {
         // TODO добавить проверку на рантайм пермишны в АПИ > 23
         PermissionChecker permissionChecker = new PermissionChecker();
-        permissionChecker.checkForPermissions(this, PERMISSION_REQUEST_FINE_LOCATION, this);
+        permissionChecker.checkForPermissions(this, PermissionChecker.RuntimePermissions.PERMISSION_REQUEST_FINE_LOCATION, this);
     }
 
     private boolean checkPlayServices() {
@@ -159,6 +175,11 @@ public class SplashScreenActivity
 
     private void startWeatherActivity() {
         if (coordinatesServicesChecked && runtimePermissionChecked && gifIsFinishedPlaying) {
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.welcome_string_disappear);
+            animation.setAnimationListener(this);
+            shimmer.cancel();
+            welcomeText.startAnimation(animation);
+
             Intent intent = new Intent(SplashScreenActivity.this, WeatherActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             Bundle bundle = ActivityOptionsCompat.makeSceneTransitionAnimation(SplashScreenActivity.this).toBundle();
@@ -169,7 +190,7 @@ public class SplashScreenActivity
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_FINE_LOCATION.VALUE) {
+        if (requestCode == PermissionChecker.RuntimePermissions.PERMISSION_REQUEST_FINE_LOCATION.VALUE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Log.d("PERMISSION", "Splash screen checkForPermissions granted");
             } else {
@@ -190,5 +211,27 @@ public class SplashScreenActivity
     @Override
     public void permissionDenied(PermissionChecker.RuntimePermissions permission) {
 
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        welcomeText.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationCompleted() {
+        gifDrawable.stop();
+        gifIsFinishedPlaying = true;
+        startWeatherActivity();
     }
 }
