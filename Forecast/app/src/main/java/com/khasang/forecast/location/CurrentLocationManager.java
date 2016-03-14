8ae10ea360b5a73a17c535aa024081df0e3de722
Роман Новоселов <novoselov_roman@mail.ru>
2016-03-14 20:12:30 +0300

    Убрал лишнее

--- Forecast/app/src/main/java/com/khasang/forecast/location/CurrentLocationManager.java
+++ Forecast/app/src/main/java/com/khasang/forecast/location/CurrentLocationManager.java
@@ -1,18 +1,13 @@
 package com.khasang.forecast.location;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
