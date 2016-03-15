e554b6bcfa2c3f53bd4aef025890ef3113b722ea
RNOVOSELOV <novoselov_roman@mail.ru>
2016-03-15 09:29:50 +0300

    Исправил баг с неправильным приветствием с 00:00 по 03:59

--- Forecast/app/src/main/java/com/khasang/forecast/activities/SplashScreenActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/SplashScreenActivity.java
@@ -54,7 +54,8 @@ public class SplashScreenActivity
         if (!welcomeStringIsOff) {
             int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
             String text = getString(R.string.welcome_string_hello);
-            if (hour >= 4 && hour <= 9) {
+            if (hour < 4) {
+            } else if (hour >= 4 && hour <= 9) {
                 text = getString(R.string.welcome_string_morning);
             } else if (hour <= 17) {
                 text = getString(R.string.welcome_string_day);
