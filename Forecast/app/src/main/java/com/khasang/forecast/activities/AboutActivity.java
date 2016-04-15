d8b80bdd90e3facb94f44fb13ec0934175054725
Vlad <vlad@allfound.ru>
2016-04-15 16:06:17 +0300

    add AboutActivity

---
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/AboutActivity.java
@@ -0,0 +1,11 @@
+package com.khasang.forecast.activities;
+
+/*
+ * EditClientProfileSecondFragment.java     15.04.2016
+ *
+ * Copyright (c) 2016 Vladislav Laptev,
+ * All rights reserved. Used by permission.
+ */
+
+public class AboutActivity {
+}
