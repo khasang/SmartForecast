f34cfacdc0d6fa312cce7d030821c2e62e974522
Vlad <vlad@allfound.ru>
2016-04-15 16:50:45 +0300

    add TeamFragment

--- Forecast/app/src/main/java/com/khasang/forecast/activities/AboutActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/AboutActivity.java
@@ -7,5 +7,77 @@ package com.khasang.forecast.activities;
  * All rights reserved. Used by permission.
  */
 
-public class AboutActivity {
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.khasang.forecast.R;
+import com.khasang.forecast.fragments.TeamFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.about_tabs);
+        ViewPager viewPager  = (ViewPager) findViewById(R.id.about_viewpager);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TeamFragment(),
+                getResources().getString(R.string.tab_name_team));
+        viewPager.setAdapter(adapter);
+    }
+
+    class TabsAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public TabsAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+    }
 }
---
+++ Forecast/app/src/main/java/com/khasang/forecast/fragments/TeamFragment.java
@@ -0,0 +1,27 @@
+package com.khasang.forecast.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.khasang.forecast.R;
+
+/*
+ * EditClientProfileSecondFragment.java     15.04.2016
+ *
+ * Copyright (c) 2016 Vladislav Laptev,
+ * All rights reserved. Used by permission.
+ */
+
+public class TeamFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_team, container, false);
+        return view;
+    }
+
+}
