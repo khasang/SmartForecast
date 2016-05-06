914a6f6690698974934a24f46e76491d482a69f6
RNOVOSELOV <novoselov_roman@mail.ru>
2016-05-06 09:52:49 +0300

    Цвет графика берется из текущей темы темы

--- Forecast/app/src/main/java/com/khasang/forecast/chart/WeatherLineDataSet.java
+++ Forecast/app/src/main/java/com/khasang/forecast/chart/WeatherLineDataSet.java
@@ -4,20 +4,22 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.utils.Utils;
 import com.khasang.forecast.R;
+
 import java.util.List;
 
 /**
  * Created by Erdeni Erdyneev on 15.04.16.
- *
+ * <p/>
  * LineDataSet для отображения данных погоды.
- *
+ * <p/>
  * index - это индекс Entry элемента в DataSet.
  * xIndex - это индекс элемента по оси X на графике.
- *
+ * <p/>
  * Вики по настройкам DataSet https://github.com/PhilJay/MPAndroidChart/wiki/DataSet-classes-in-detail
  */
 public class WeatherLineDataSet extends LineDataSet {
@@ -27,8 +29,8 @@ public class WeatherLineDataSet extends LineDataSet {
     private Entry yMaxEntry;
 
     public WeatherLineDataSet(List<Entry> yVals, Context context) {
-    this(yVals, "", context);
-  }
+        this(yVals, "", context);
+    }
 
     public WeatherLineDataSet(List<Entry> yVals, String label, Context context) {
         super(yVals, label);
@@ -60,13 +62,12 @@ public class WeatherLineDataSet extends LineDataSet {
         setColor(ContextCompat.getColor(context, R.color.chart_line)); // цвет линии
         setLineWidth(context.getResources().getDimension(R.dimen.chart_line_width)); // толщина линии
 
-      setDrawFilled(true); // разрешаем заливку цвета под графиком
-      if (Utils.getSDKInt() >= 18) {
-          Drawable drawable = ContextCompat.getDrawable(context, R.drawable.chart_fade);
-          setFillDrawable(drawable);
-        }
-        else {
-          setFillColor(ContextCompat.getColor(context, R.color.primary_light));
+        setDrawFilled(true); // разрешаем заливку цвета под графиком
+        if (Utils.getSDKInt() >= 18) {
+            Drawable drawable = ContextCompat.getDrawable(context, R.drawable.chart_fade);
+            setFillDrawable(drawable);
+        } else {
+            setFillColor(ContextCompat.getColor(context, R.color.accent));
         }
 
         // разрешаем закругление линии
@@ -75,11 +76,11 @@ public class WeatherLineDataSet extends LineDataSet {
         setDrawCubic(true);
         setCubicIntensity(0.1f);
 
-        setValueTextSize(context.getResources().getDimension(R.dimen.chart_value_size)); // размер текста
-        setDrawCircleHole(false); // запрещаем кружок внутри кружка
+        setValueTextSize(context.getResources().getDimension(R.dimen.chart_value_size));    // размер текста
+        setDrawCircleHole(false);   // запрещаем кружок внутри кружка
 
         int[] colors = getColors(getYVals());
-        setCircleColors(colors); // цвета кружков
+        setCircleColors(colors);    // цвета кружков
     }
 
     private int[] getColors(List<Entry> yValues) {
