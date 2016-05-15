dd0fff4e1f57b95977a747dad78b8b800f0bdc01
Erdeni Erdyneev <erdeni.bat@gmail.com>
2016-05-16 00:29:54 +0600

    Убрал header картинку, добавил темы

--- Forecast/app/src/main/java/com/khasang/forecast/activities/AboutActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/AboutActivity.java
@@ -1,20 +1,19 @@
 package com.khasang.forecast.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
-
 import com.khasang.forecast.R;
 import com.khasang.forecast.fragments.FeaturesFragment;
-import com.khasang.forecast.fragments.VersionsFragment;
 import com.khasang.forecast.fragments.TeamFragment;
-
+import com.khasang.forecast.fragments.VersionsFragment;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,29 +23,42 @@ import java.util.List;
  * Copyright (c) 2016 Vladislav Laptev,
  * All rights reserved. Used by permission.
  */
-
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String colorScheme = sp.getString(getString(R.string.pref_color_scheme_key), getString(R.string.pref_color_scheme_teal));
+        if (colorScheme.equals(getString(R.string.pref_color_scheme_brown))) {
+            setTheme(R.style.AppTheme_About_Brown);
+        } else if (colorScheme.equals(getString(R.string.pref_color_scheme_teal))) {
+            setTheme(R.style.AppTheme_About_Teal);
+        } else if (colorScheme.equals(getString(R.string.pref_color_scheme_indigo))) {
+            setTheme(R.style.AppTheme_About_Indigo);
+        } else if (colorScheme.equals(getString(R.string.pref_color_scheme_purple))) {
+            setTheme(R.style.AppTheme_About_Purple);
+        } else {
+            setTheme(R.style.AppTheme_About_Green);
+        }
         setContentView(R.layout.activity_about);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.about_tabs);
         ViewPager viewPager  = (ViewPager) findViewById(R.id.about_viewpager);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.about_toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle(" ");
-            //actionBar.setSubtitle("Khasang Open Source");
-        }
-
+        setupToolbar();
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     private void setupViewPager(ViewPager viewPager) {
