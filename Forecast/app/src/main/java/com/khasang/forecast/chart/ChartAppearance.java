2aa4d8ffcd9e0ed23a7d8d273fcd1f0c5e499617
Erdeni Erdyneev <erdeni.bat@gmail.com>
2016-06-02 00:08:51 +0600

    Добавил анимацию появления и исчезновения графика

--- Forecast/app/src/main/java/com/khasang/forecast/chart/ChartAppearance.java
+++ Forecast/app/src/main/java/com/khasang/forecast/chart/ChartAppearance.java
@@ -7,6 +7,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.FrameLayout;
 
 import com.khasang.forecast.R;
@@ -25,14 +27,59 @@ public class ChartAppearance {
         setFabOnTopBehavior();
     }
 
+    public void expand(final View v) {
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = (int) (chartHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int) (chartHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
+    public void collapse(final View v) {
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if (interpolatedTime == 1) {
+                    v.setVisibility(View.GONE);
+                } else {
+                    v.getLayoutParams().height = chartHeight - (int) (chartHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int) (chartHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
+    }
+
     private void setFabOnTopBehavior() {
         CoordinatorLayout.LayoutParams fabLayoutParams = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
         fabLayoutParams.setBehavior(new FabOnTopBehavior());
         fabLayoutParams.setAnchorId(R.id.appbar);
         fabLayoutParams.anchorGravity = Gravity.BOTTOM | Gravity.END;
 
-        chatLayout.getLayoutParams().height = 0;
         fab.show();
+        collapse(chatLayout);
     }
 
     private void setFabOnBottomBehavior() {
@@ -41,8 +88,8 @@ public class ChartAppearance {
         fabLayoutParams.setAnchorId(R.id.chart_layout);
         fabLayoutParams.anchorGravity = Gravity.TOP | Gravity.END;
 
-        chatLayout.getLayoutParams().height = chartHeight;
         fab.show();
+        expand(chatLayout);
     }
 
     public class FabOnTopBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
