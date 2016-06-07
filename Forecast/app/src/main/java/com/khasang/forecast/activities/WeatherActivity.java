b67450fe4e455316b5ad7864ae2e86896b3527f4
RomeoND <copypastestd@gmail.com>
2016-06-08 01:48:38 +0300

    before add DeepLink Activity

--- Forecast/app/src/main/java/com/khasang/forecast/activities/WeatherActivity.java
+++ Forecast/app/src/main/java/com/khasang/forecast/activities/WeatherActivity.java
@@ -40,7 +40,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInvite;
 import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.appinvite.AppInviteInvitationResult;
+import com.google.android.gms.appinvite.AppInviteReferral;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.khasang.forecast.AppUtils;
 import com.khasang.forecast.Logger;
 import com.khasang.forecast.MyApplication;
@@ -75,7 +81,7 @@ import static com.khasang.forecast.PermissionChecker.RuntimePermissions.PERMISSI
  */
 public class WeatherActivity extends AppCompatActivity
         implements View.OnClickListener, SwipeRefreshLayout.OnRefreshListener, IWeatherReceiver,
-        IPermissionCallback, IMessageProvider, NavigationDrawer.OnNavigationItemClickListener {
+        IPermissionCallback, IMessageProvider, NavigationDrawer.OnNavigationItemClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private static final int CHOOSE_CITY = 1;
     public static final String CURRENT_CITY_TAG = "CURRENT_CITY";
@@ -99,6 +105,10 @@ public class WeatherActivity extends AppCompatActivity
     private WeatherChart chart;
     private NavigationDrawer navigationDrawer;
     private FrameLayout chartLayout;
+
+    // [START define_variables]
+    private GoogleApiClient mGoogleApiClient;
+    // [END define_variables]
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -158,6 +168,43 @@ public class WeatherActivity extends AppCompatActivity
                     .hide(dailyForecastFragment)
                     .commit();
         }
+
+
+        // Create an auto-managed GoogleApiClient with access to App Invites.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(AppInvite.API)
+                .enableAutoManage(this, this)
+                .build();
+
+        // Check for App Invite invitations and launch deep-link activity if possible.
+        // Requires that an Activity is registered in AndroidManifest.xml to handle
+        // deep-link URLs.
+        boolean autoLaunchDeepLink = true;
+        AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, this, autoLaunchDeepLink)
+                .setResultCallback(
+                        new ResultCallback<AppInviteInvitationResult>() {
+                            @Override
+                            public void onResult(AppInviteInvitationResult result) {
+                                Log.d(TAG, "getInvitation:onResult:" + result.getStatus());
+                                if (result.getStatus().isSuccess()) {
+                                    // Extract information from the intent
+                                    Intent intent = result.getInvitationIntent();
+                                    String deepLink = AppInviteReferral.getDeepLink(intent);
+                                    String invitationId = AppInviteReferral.getInvitationId(intent);
+
+                                    // Because autoLaunchDeepLink = true we don't have to do anything
+                                    // here, but we could set that to false and manually choose
+                                    // an Activity to launch to handle the deep link here.
+                                    // ...
+                                }
+                            }
+                        });
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        showMessage(getString(R.string.google_play_services_error));
     }
 
     private void initFields() {
